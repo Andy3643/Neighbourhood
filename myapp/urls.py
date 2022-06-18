9991efba3df2fe43c9905fdbc8933552c764ab88
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-06-18 11:59:22 +0300

    ft:add user registration function to add new users to the database.

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path,include
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 
 urlpatterns = [
     path('',views.Index_view,name="home"),
+    path('sign-up/',views.register,name="register"),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import *
 # Create your views here.
 def Index_view(request):
       return render(request,"index.html")   
+  
+def register(request):
+    '''
+    Function to register new users to the database.
+    '''
+    if request.method == 'POST':
+        form =UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f"You have succesfully created an account. Proceed to Login")
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    context = {
+        'form':form
+    }
+    return render(request,"users/register.html",context)
